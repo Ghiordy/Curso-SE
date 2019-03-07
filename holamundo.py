5cb81bc14530b23f43d1597590f7b6b9ff35f541
Ghiordy <cghiordy@gmail.com>
2019-03-07 13:39:59 -0500

    Update and rename holamunfo.py to holamundo.py

---
+++ holamundo.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar  11:28:24 2019
+
+@author: Ghiordy F. Contreras
+"""
+
+
+import RPi.GPIO as GPIO
+import time 
+
+GPIO.setwarnings(False)
+
+pin = 21
+n = 2
+delay = 2
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(pin,GPIO.OUT)
+
+def encenderLed(pin,n,delay):
+    print("Hello world: Ejecutado ",n,
+          " veces por ",delay*n," segundos")
+    while (n > 0):
+        GPIO.output(pin,True)
+        time.sleep(delay)
+        GPIO.output(pin,False)
+        time.sleep(delay)
+        n = n - 1
+        print("Queda ",n*delay," segundos ...")
+    print("Finalizado")
+
+encenderLed(pin,2,delay)
