2a8a6cc34e6cf8b1e9b4763a1bb44d68778cccf8
Ghiordy <cghiordy@gmail.com>
2019-03-11 16:15:19 -0500

    Add files via upload

---
+++ restablecer.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar  16:10:24 2019
+@author: Ghiordy F. Contreras
+"""
+
+import time
+print('Borrando scripts anteriores en el',time.strftime("%c"))
+
+
+''' Reestablece los pines cuando no se sabe
+que programa ha sido cargado previamente y evita
+danar algun dispositivo nuevo al conectar en el GPIO'''
