b1359b85ac9c2b37e9bcf9a54e380773a71b1999
Ghiordy <cghiordy@gmail.com>
2019-03-11 17:18:22 -0500

    Add files via upload

--- restablecer.py
+++ restablecer.py
@@ -5,8 +5,19 @@ Created on Mon Mar  16:10:24 2019
 """
 
 import time
+import RPi.GPIO as GPIO
 print('Borrando scripts anteriores en el',time.strftime("%c"))
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+def restablecer():
+    for i in range(27):
+        GPIO.setup(i,GPIO.OUT)
+        GPIO.output(i,False)
+        print('Pin ',i,'restablecido')
+    print('Terminado: ',time.strftime("%c"))
+    return 'Hecho!'
 
+restablecer()
 
 ''' Reestablece los pines cuando no se sabe
 que programa ha sido cargado previamente y evita
