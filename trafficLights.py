16c7b60bd36b69d893abcecc5c1feb4073fc3d9c
Ghiordy <cghiordy@gmail.com>
2019-03-14 22:43:16 -0500

    onlines state machine examples

---
+++ trafficLights.py
@@ -0,0 +1,14 @@
+from statemachine import StateMachine, State
+
+class TrafficLightMachine(StateMachine):
+    green = State('Green', initial=True)
+    yellow = State('Yellow')
+    red = State('Red')
+
+    slowdown = green.to(yellow)
+    stop = yellow.to(red)
+    go = red.to(green)
+    
+traffic_light = TrafficLightMachine()
+
+#traffic_light.current_state
