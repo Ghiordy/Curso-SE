33ac42001a4a3e5487ce318c31d66184910a68c7
Ghiordy <cghiordy@gmail.com>
2019-03-19 07:47:42 -0500

    Maquina moore FSM para detector 101funcionando

--- detector101moore.py
+++ detector101moore.py
@@ -10,11 +10,11 @@ import numpy as np
 
 #rebote = 0.3
 
-def configurar(entrada,salida,pare,habilita):
+def configurar(pulsador0,pulsador1,pare,salida):
     GPIO.setmode(GPIO.BCM)
     GPIO.setwarnings(False)
-    GPIO.setup(entrada,GPIO.IN)
-    GPIO.setup(habilita,GPIO.IN)
+    GPIO.setup(pulsador0,GPIO.IN)
+    GPIO.setup(pulsador1,GPIO.IN)
     GPIO.setup(pare,GPIO.IN)
     GPIO.setup(salida,GPIO.OUT)
     #estados = [0,1,2,3]
@@ -32,37 +32,40 @@ def decoSalida(estado,salida):
     if estado == 2:
         GPIO.output(salida,0)
     if estado == 3:
-        GPIO.outpu(salida,1)
+        GPIO.output(salida,1)
     return estado == 3
 
-def decoEstado(habilita,entrada,estado,rebote):
-    s = leer(habilita)
-    i = leer(entrada)
+def decoEstado(pulsador0,pulsador1,estado,rebote,estados):
+    p0 = leer(pulsador0)
+    p1 = leer(pulsador1)
     time.sleep(rebote)
-    if s:
-        if estado == 0 and i:
-            estado = 1
-        elif estado == 0 and not(i):
-            estado = 0
-        if estado == 1 and not(i):
-            estado = 2
-        elif estado == 1 and i:
-            estado = 1
-        if estado == 2 and i:
-            estado = 3
-        elif estado == 2 and not(i):
-            estado = 1
-        if estado == 3 and not(i):
-            estado = 2
-        elif estado == 3 and i:
-            estado = 1
+    if estado == estados[0]:
+        if p0 == 1:
+            estado = estados[0]
+        elif p1 == 1:
+            estado = estados[1]
+    elif estado == estados[1]:
+        if p0 == 1:
+            estado = estados[2]
+        elif p1 == 0:
+            estado = estados[1]
+    elif estado == estados[2]:
+        if p0 == 1:
+            estado = estados[0]
+        elif p1 == 1:
+            estado = estados[3]
+    elif estado == estados[3]:
+        if p0 == 1:
+            estado = estados[2]
+        elif p1 == 1:
+            estado = estados[1]
     return estado
 
-def main(entrada,habilita,salida,rebote,pare):
+def main(pulsador0,pulsador1,salida,rebote,pare,estados):
     print('Instante: ',time.strftime("%c"))
-    estado = configurar(entrada,salida,pare,habilita)
+    estado = configurar(pulsador0,pulsador1,pare,salida)
     aciertos = 0
-    while(leer(pare) != 1):
+    while(leer(pare) != 0):
         print('ESTADO: S',estado)
         so = decoSalida(estado,salida)
         if so:
@@ -70,10 +73,16 @@ def main(entrada,habilita,salida,rebote,pare):
             aciertos =+1
         else:
             print('Por favor ingrese 101')
-        estado = decoEstado(habilita,entrada,
-                            estado,rebote)
+        estado = decoEstado(pulsador0,pulsador1,estado,rebote,estados)
     print('Se han logrado ',aciertos,' detecciones')
     return aciertos
 
-main(2,3,4,1,14)
+main(2,3,4,1,14,[0,1,2,3])
+# pulsador0 = 2
+# pulsador1 = 3
+# rebote = 1
+# pare = 14
+# estados
+
+
     
