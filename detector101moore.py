b2c9310ccee084e08996ff17f46f95e7583f1f51
Ghiordy <cghiordy@gmail.com>
2019-03-18 23:10:25 -0500

    Moore ready

--- detector101moore.py
+++ detector101moore.py
@@ -8,20 +8,21 @@ import RPi.GPIO as GPIO
 import time
 import numpy as np
 
-rebote = 0.3
+#rebote = 0.3
 
-def setup(entrada,salida,habilita):
+def configurar(entrada,salida,pare,habilita):
     GPIO.setmode(GPIO.BCM)
     GPIO.setwarnings(False)
     GPIO.setup(entrada,GPIO.IN)
     GPIO.setup(habilita,GPIO.IN)
+    GPIO.setup(pare,GPIO.IN)
     GPIO.setup(salida,GPIO.OUT)
-    estados = [0,1,2,3]
+    #estados = [0,1,2,3]
     estado = 0
     return estado
 
-def leer(entrada,rebote):
-    return GPIO.input(entrada)
+def leer(pin):
+    return GPIO.input(pin)
 
 def decoSalida(estado,salida):
     if estado == 0:
@@ -34,12 +35,43 @@ def decoSalida(estado,salida):
         GPIO.outpu(salida,1)
     return estado == 3
 
-def decoEstado(habilita,entrada,estado,salida):
+def decoEstado(habilita,entrada,estado,rebote):
     s = leer(habilita)
-    print('senal: ',s,' tipo ',type(s))
-    i == leer(entrada)
-    print('entrada: ',i,' tipo ',type(i))
+    i = leer(entrada)
     time.sleep(rebote)
-    if senal:
-        if estado == 0 and i
-    return False
+    if s:
+        if estado == 0 and i:
+            estado = 1
+        if estado == 1 and not(i):
+            estado = 2
+        elif estado == 1 and i:
+            estado = 1
+        if estado == 2 and i:
+            estado = 3
+        elif estado == 2 and not(i):
+            estado = 1
+        if estado == 3 and not(i):
+            estado = 2
+        elif estado == 3 and i:
+            estado = 1
+    return estado
+
+def main(entrada,habilita,salida,rebote,pare):
+    print('Instante: ',time.strftime("%c"))
+    estado = configurar(entrada,salida,pare,habilita)
+    aciertos = 0
+    while(leer(pare) != 1):
+        estado = decoEstado(habilita,entrada,
+                            estado,rebote)
+        print('ESTADO: S',estado)
+        so = decoSalida(estado,salida)
+        if so:
+            print('Secuencia detectada')
+            aciertos =+1
+        else:
+            print('Por favor ingrese 101')
+    print('Se han logrado ',aciertos,' detecciones')
+    return aciertos
+
+main(2,3,4,0.3,14)
+    
