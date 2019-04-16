1476d18ce46f5f20ac990623924771ad9cc7646b
Ghiordy <cghiordy@gmail.com>
2019-04-16 11:50:10 -0500

    RPi3 updated

---
+++ RPi3-FSM/led.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr  10:45:24 2019
+@author: Ghiordy F. Contreras
+"""
+class led:
+    def __init__(self):
+        state = 0
+        states = [0,1]
+        
+    def chState(newState):
+        return states[newState]
+    
